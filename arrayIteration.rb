3e3b6551f07e0f7cb592402ccb5ebcd27c0d1133
Pablo <pablito@me.com>
2023-02-22 00:48:29 +0000

    Added to arrayIteration.rb

--- arrayIteration.rb
+++ arrayIteration.rb
@@ -5,3 +5,11 @@ while current_index < my_array.length do
   puts "I'm looping! Loop #{current_index + 1}"
   current_index += 1
 end
+
+my_array = ["Hello", "there", "friend!"]
+current_index = 0
+
+while current_index < my_array.length do
+  puts my_array[current_index]
+  current_index += 1
+end
