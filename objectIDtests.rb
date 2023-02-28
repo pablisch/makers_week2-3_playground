2157f3321b0cbd9be30b761c1c0be26f2545bd60
Pablo <pablito@me.com>
2023-02-28 14:18:49 +0000

    More objectIDtests.rb

--- objectIDtests.rb
+++ objectIDtests.rb
@@ -1,39 +1,50 @@
-thing1 = "one"
-puts thing1 # => one
-puts thing1.object_id # => 60
+# thing1 = "one"
+# puts thing1 # => one
+# puts thing1.object_id # => 60
 
-thing2 = thing1
-puts thing2 # => one
-puts thing2.object_id # => 60
+# thing2 = thing1
+# puts thing2 # => one
+# puts thing2.object_id # => 60
 
-thing1[3] = "s"
-puts thing2 # => ones
-puts thing2.object_id # => 60
+# thing1[3] = "s"
+# puts thing2 # => ones
+# puts thing2.object_id # => 60
 
-thing1 = "ones"
-puts thing1 # => ones
-puts thing1.object_id # => 80
-puts thing2 # => ones
-puts thing2.object_id # => 60
+# thing1 = "ones"
+# puts thing1 # => ones
+# puts thing1.object_id # => 80
+# puts thing2 # => ones
+# puts thing2.object_id # => 60
 
-thing10 = "ones"
-puts thing10 # => ones
-puts thing10.object_id # => 100
+# thing10 = "ones"
+# puts thing10 # => ones
+# puts thing10.object_id # => 100
 
-puts
+# puts
 
-thing3 = 1
-puts thing3 # => 1
-puts thing3.object_id # => 3
-thing4 = thing3
-puts thing4 # => 1
-puts thing4.object_id # => 3
-thing5 = 1
-puts thing5 # => 1
-puts thing5.object_id # => 3
+# thing3 = 1
+# puts thing3 # => 1
+# puts thing3.object_id # => 3
+# thing4 = thing3
+# puts thing4 # => 1
+# puts thing4.object_id # => 3
+# thing5 = 1
+# puts thing5 # => 1
+# puts thing5.object_id # => 3
 
-thing3 += 1
-puts thing3 # => 2
-puts thing3.object_id # => 5
-puts thing4 # => 1
-puts thing4.object_id # => 3
+# thing3 += 1
+# puts thing3 # => 2
+# puts thing3.object_id # => 5
+# puts thing4 # => 1
+# puts thing4.object_id # => 3
+
+string1 = String.new("Hello")
+
+string2 = string1
+string3 = string2
+
+p string1.object_id # => 60
+p string2.object_id # => 60
+p string3.object_id # => 60
+
+p "Hello".object_id # => 
