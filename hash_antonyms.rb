e814e002d6978ef167d099329800522066c64d4e
Pablo <pablito@me.com>
2023-03-03 19:18:45 +0000

    Added hash_antonyms.rb

---
+++ hash_antonyms.rb
@@ -0,0 +1,12 @@
+ANTONYMS = {
+  "right" => "left",
+  "big" => "small",
+  "up" => "down",
+  "north" => "south",
+  "east" => "west",
+  "naughty" => "nice",
+  "black" => "white",
+  "good" => "evil",
+}
+
+KEYS = ANTONYMS.each
